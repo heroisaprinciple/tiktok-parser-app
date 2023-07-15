c4ed49edb7b411854781c124007b6a644bdee2c9
Arina <valcat552@gmail.com>
2023-07-15 16:43:50 +0100

    refactored to class

--- scraper.rb
+++ scraper.rb
@@ -1,93 +1,103 @@
 require 'csv'
-require 'byebug'
 require 'nokogiri'
 require 'httparty'
-require 'json'
-require 'open-uri'
 require 'selenium-webdriver'
-require 'selenium/webdriver/common/wait'
 
-def find_user_data(query, number)
-  url = query.include?('#') ? "https://www.tiktok.com/tag/#{query}" : "https://www.tiktok.com/search?q=#{query}"
-
-  driver = Selenium::WebDriver.for :chrome
-  wait = Selenium::WebDriver::Wait.new(timeout: 30)
-  driver.get(url)
-
-  data = []
-  i = 0
-
-  while i < number
-    quote_container = wait.until { driver.find_elements(css: '.tiktok-hbrxqe-DivVideoSearchCardDesc.etrd4pu0') }
-
-    quote_container.each do |container|
-      name = container.find_element(css: '.tiktok-2zn17v-PUniqueId.etrd4pu6').attribute('textContent')
-      user_url = "https://www.tiktok.com/@#{name}"
-      res = HTTParty.get(user_url)
-      docs = Nokogiri::HTML(res.body)
-
-      followers_count = find_followers_amount(docs)
-      avg_views = find_average_views(docs)
-      description = find_description(docs)
-      email = find_email(description)
-      social_accounts = find_socials(description)
-
-      data << [name, followers_count, avg_views, description, email, social_accounts]
-      p data
-      i += 1
-      break if i >= number
-    end
+class TikTokParser
+  def initialize
+    @driver = Selenium::WebDriver.for :chrome
+    @wait = Selenium::WebDriver::Wait.new(timeout: 30)
   end
 
-  driver.quit
-  data
-end
+  def scrape_user_data(query, number)
+    url = query.include?('#') ? "https://www.tiktok.com/tag/#{query}" : "https://www.tiktok.com/search?q=#{query}"
+    @driver.get(url)
 
-def find_name(container)
-  container.find_element(css: '.tiktok-2zn17v-PUniqueId.etrd4pu6').attribute('textContent')
-end
+    data = []
+    i = 0
 
-def find_followers_amount(docs)
-  account_els = docs.css('.tiktok-rxe1eo-DivNumber strong')
-  followers_element = account_els.find { |el| el.attribute('title').value == 'Followers' }
-  followers_element.text
-end
+    while i < number
+      quote_container = @wait.until { @driver.find_elements(css: '.tiktok-hbrxqe-DivVideoSearchCardDesc.etrd4pu0') }
 
-def find_average_views(docs)
-  avg_nums = docs.css('.video-count')
-  avg_nums.map { |el| el.text.to_f }.sum / avg_nums.size
-end
+      quote_container.each do |container|
+        name = find_name(container)
+        user_url = "https://www.tiktok.com/@#{name}"
+        res = HTTParty.get(user_url)
+        docs = Nokogiri::HTML(res.body)
 
-def find_description(docs)
-  description = docs.css('.tiktok-vdfu13-H2ShareDesc.e1457k4r3')
-  description.text
-end
+        followers_count = find_followers_amount(docs)
+        avg_views = find_average_views(docs)
+        description = find_description(docs)
+        email = find_email(description)
+        social_accounts = find_socials(description)
 
-def find_email(desc)
-  desc.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}\b/i).join
-end
+        data << [name, followers_count, avg_views, description, email, social_accounts]
+        p data
+        i += 1
+        break if i >= number
+      end
+    end
 
-def find_socials(desc)
-  desc.scan(/(Twitter|IG|Insta(?:gram)?|Snapchat|Skype|Youtube|Discord): ?\(?@?(\w+)\)?/i)
-      .map { |network, username| "#{network}: #{username}" }
-      .join(' ')
-      .gsub(/:\s/, ':')
-end
+    @driver.quit
+    data
+  end
 
-def generate_csv(data)
-  CSV.open('tiktoker_followers.csv', 'w+',
-           write_headers: true,
-           headers: %w[Account Followers Avg_Views Channel_Desc Email Other_Accounts]) do |csv|
-    data.each do |row|
-      csv << row
+  def call_generate_csv(data)
+    generate_csv(data)
+  end
+
+  private
+
+  def find_name(container)
+    container.find_element(css: '.tiktok-2zn17v-PUniqueId.etrd4pu6').attribute('textContent')
+  end
+
+  def find_followers_amount(docs)
+    account_els = docs.css('.tiktok-rxe1eo-DivNumber strong')
+    followers_element = account_els.find { |el| el.attribute('title').value == 'Followers' }
+    followers_element.text
+  end
+
+  def find_average_views(docs)
+    avg_nums = docs.css('.video-count')
+    avg_nums.map { |el| el.text.to_f }.sum / avg_nums.size
+  end
+
+  def find_description(docs)
+    description = docs.css('.tiktok-vdfu13-H2ShareDesc.e1457k4r3')
+    description.text
+  end
+
+  def find_email(desc)
+    desc.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}\b/i).join
+  end
+
+  def find_socials(desc)
+    desc.scan(/(Twitter|IG|Insta(?:gram)?|Snapchat|Skype|Youtube|Discord): ?\(?@?(\w+)\)?/i)
+        .map { |network, username| "#{network}: #{username}" }
+        .join(' ')
+        .gsub(/:\s/, ':')
+  end
+
+  def generate_csv(data)
+    CSV.open('tiktoker_followers.csv', 'w+', write_headers: true,
+             headers: %w[Account Followers Avg_Views Channel_Desc Email Other_Accounts]) do |csv|
+      data.each do |row|
+        csv << row
+      end
     end
   end
 end
+
+# tags: 'https://www.tiktok.com/tag/askingquestions'
+
+# notitsmenicksmithy or jamesdoylefitness or bobbysolez or kerana0208 or yungalyy
 
 puts 'Enter the query to use:'
 query = gets.chomp
 puts 'Enter the number of users and info about them you want to see (e.g., 10):'
 number = gets.chomp.to_i
 
-data = find_user_data(query, number)
-generate_csv(data)
+scraper = TikTokParser.new
+data = scraper.scrape_user_data(query, number)
+scraper.call_generate_csv(data)
